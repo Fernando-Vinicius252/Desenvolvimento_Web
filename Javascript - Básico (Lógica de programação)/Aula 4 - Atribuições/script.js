a202dc438f7418d12464c39c67a9ec4883f57455
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-15 19:36:53 -0300

    Aula 4

---
+++ Javascript - Básico (Lógica de programação)/Aula 4 - Atribuições/script.js
@@ -0,0 +1,17 @@
+// =    |    x = y        é igual:    x = y
+// +=   |    x += y       é igual:    x = x + y
+// -=   |    x -= y       é igual:    x = x - y
+// *=   |    x *= y       é igual:    x = x * y
+// /=   |    x /= y       é igual:    x = x / y
+// %=   |    x %= y       é igual:    x = x % y
+
+/*
+a++ é igual a+=1
+a-- é igual a-=1
+*/
+
+var a = 30;
+
+a = a % 28;
+
+console.log(a)
