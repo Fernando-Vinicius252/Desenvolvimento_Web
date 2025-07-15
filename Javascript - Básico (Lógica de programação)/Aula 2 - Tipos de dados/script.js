1b69c06e8cbaa0cdcccf0c8277e2ea58ea64239d
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-15 19:33:47 -0300

    Aula 2

---
+++ Javascript - Básico (Lógica de programação)/Aula 2 - Tipos de dados/script.js
@@ -0,0 +1,30 @@
+// Tudo que estiver nessa linha vai ser um comentario
+
+/*
+    Tudo que estiver aqui dentro
+    Vai ser um comentário
+
+*/
+
+// ------------ Conteudo --------------- //
+
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+
+var a;              // Undefined
+var a = 1;          // Number
+var b = "texto";    // String
+var c = null;       // Null
+var d = true;       //ou false --> Boolean
+
+console.log(a);
+console.log(b);
+
+
+/*
+console.log("1" + "1");
+console.log(1 + 1);
+*/
