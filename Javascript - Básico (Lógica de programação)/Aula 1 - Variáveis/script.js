5178b257a68b75787e6d0131e6b638dd41ec1a55
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-15 19:32:44 -0300

    Aula 1

---
+++ Javascript - Básico (Lógica de programação)/Aula 1 - Variáveis/script.js
@@ -0,0 +1,15 @@
+/* alert("Hello Word") */
+console.log("Hello world")
+
+var a;
+
+a = "Placa"
+console.log(a)
+
+a = "Placa de captura"
+console.log(a)
+
+var b = "hd";
+a = "Placa de captura";
+b = a;
+console.log(b);
