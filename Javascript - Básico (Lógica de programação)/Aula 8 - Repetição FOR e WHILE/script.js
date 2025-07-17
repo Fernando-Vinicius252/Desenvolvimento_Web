a27dc54f1fd1399114f24f721f895c2c9d5258de
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-17 19:37:41 -0300

    Aula 8

---
+++ Javascript - Básico (Lógica de programação)/Aula 8 - Repetição FOR e WHILE/script.js
@@ -0,0 +1,18 @@
+var numero = Math.random() * 100
+
+
+/*
+for(var vez = 0; vez < numero; vez++){
+
+    console.log("Executando o for, pela " + vez + " Vez")
+}
+*/
+
+
+while(numero < 90){
+    console.log(numero);
+    numero = Math.random() * 100
+}
+
+console.log(numero);
+console.log("Acabou")
