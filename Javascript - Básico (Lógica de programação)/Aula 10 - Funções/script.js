3dc6d7f4cf29b12f116592d5e7c5f8375a152ca2
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-17 19:40:44 -0300

    Aula 10

---
+++ Javascript - Básico (Lógica de programação)/Aula 10 - Funções/script.js
@@ -0,0 +1,30 @@
+// ------------- Function anonima -------------
+// --------- (dentro de uma variavel) -------------
+
+var media = function (n1, n2){
+
+    return (n1 + n2) / 2;
+}
+
+console.log(media(5, 6));
+
+
+// ------------- Function declarada -------------
+/*
+function media(n1, n2) {
+    var nota1 = n1;
+    var nota2 = n2;
+    var media = (nota1 + nota2) / 2;
+    // console.log(media);
+
+    return media;
+}
+
+var resultado = media(6, 7);
+var m = media;
+
+var resultado2 = m(2 , 3);
+
+console.log(resultado)
+console.log(resultado2)
+*/
