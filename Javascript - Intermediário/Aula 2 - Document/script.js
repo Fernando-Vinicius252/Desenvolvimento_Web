9a6d4be9299a978f4b6a1b66795ff1b584d5f5bb
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-22 04:48:54 -0300

    Aula 2

---
+++ Javascript - Intermediário/Aula 2 - Document/script.js
@@ -0,0 +1,11 @@
+console.log(document);
+
+/* --------------- executar via console ---------------
+
+console.log(document.body);
+
+document.body.innerHTML = "<h1>Esse título foi inserido no JS</h1>" + document.body.innerHTML
+
+console.log(document.body);
+
+*/
