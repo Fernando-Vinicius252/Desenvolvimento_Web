d2d4b8636504574b45434507c77a686b728cf95d
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-22 04:47:35 -0300

    Aula 1

---
+++ Javascript - Intermediário/Aula 1 - Window/script.js
@@ -0,0 +1,8 @@
+/*
+Tudo no Js advém do objeto "window". - Objeto padrão de uma pagina.
+
+Portanto na grande maioria dos casos não é necessario chamar este objeto para utilizar dos seus metodos ou propriedades.
+
+em caso de duvidas, eu posso visualizar esses metodos e propriedades escrevendo dentro do console do navegador a palavra: Window
+
+*/
