24afae2f7ea831d409196d0483da874eda37f303
Fernando-Vinicius252 <fernando_vinicius252@hotmail.com>
2025-07-22 04:49:56 -0300

    Aula 3

---
+++ Javascript - Intermediário/Aula 3 - Acessando elementos HTML/script.js
@@ -0,0 +1,17 @@
+console.log(document);
+
+/* --------------- executar via console no navegador ---------------
+
+let p1 = document.getElementById("p1") // atribui a variavel 'p1' o elemento que contem o id = "p1" dentro do document.
+
+console.log(p1) // imprime a variavel p1
+
+let paragrafos = document.getElementsByClassName("Paragrafo") // atribui a variavel 'paragrafos' todos os elementos que possuem a class = "Paragrafo" dentro do document, em Array.
+
+console.log(paragrafos) // imprime a variavel paragrafo(em Array)
+
+paragrafos // chamando a variavel diretamente.
+
+let tp = document.getElementsByTagName("p") // atribui a variavel 'tp' todos os elementos com a tag <"p"> dentro do document, em Array.
+
+*/
